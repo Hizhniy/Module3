b8cfdfeee8c92e3fa7e330d9ed767e45129011f8
Hizhniy <hizhniy@gmail.com>
2023-04-15 18:22:25 +0300

    Программа 3.2.1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,19 +2,14 @@
 
 namespace FirstApplication.ConsoleApp // Объявление пространства имен для данного кода
 {
-    class Program // Объявление класса Program
+    class MainClass
     {
-        static void Main(string[] args) // Объявление метода Main
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Как Вас зовут?");
-            string name = Console.ReadLine();
-            Console.WriteLine("Здравствуйте, {0}", name);
-            Console.WriteLine("А вес?");
-            string weight = Console.ReadLine();
-            Console.WriteLine("Ого, целых {0}", weight);
-            Console.WriteLine("А рост?");
-            string height = Console.ReadLine();
-            Console.WriteLine("Ничего себе! {0} см", height);
+            string myName;
+            myName = "Илья";
+            Console.WriteLine(myName);
+            Console.ReadKey();
         }
     }
 }
